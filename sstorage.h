a37c5301d725271e703c9e0962f705140d1d8a8d
Jeff Richards <jeff.richards@tradingtechnologies.com>
2017-04-04 10:24:11 -0500

    initial load

--- sstorage.h
+++ sstorage.h
@@ -1,25 +1,3 @@
-/***************************************************************************
- *    
- *                Unpublished Work Copyright (c) 2001 - 2011
- *                  Trading Technologies International, Inc.
- *                       All Rights Reserved Worldwide
- *
- *          * * *   S T R I C T L Y   P R O P R I E T A R Y   * * *
- *
- * WARNING:  This program (or document) is unpublished, proprietary property
- * of Trading Technologies International, Inc. and is to be maintained in
- * strict confidence. Unauthorized reproduction, distribution or disclosure 
- * of this program (or document), or any program (or document) derived from
- * it is prohibited by State and Federal law, and by local law outside of 
- * the U.S.
- *
- ***************************************************************************
- * $Source: coreclass.h $
- * $Date: 2011/10/28 09:46:01CDT $
- * $Author: Erica Bradnick (TT) (ebradnick) $
- * $Revision: 2.27 $
- ***************************************************************************/
- 
 #pragma once
 
 #ifndef __IDEMPOTENT_TRANSACTION_COUNTING_SSTORAGE_H_
